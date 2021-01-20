588088a6be4a80c412acf2e2a615eab1c9fa9ddc
seperina <seperina.ji@gmail.com>
2021-01-21 01:34:05 +0800

    Added Week2 Fashion MNIST case

---
+++ Introduction To TensorFlow for Artificial Intelligence,Machine Learning and Deep Learning /Week2/FashionMinist.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+print(tf.__version__)
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('loss') < 0.05):
+            print("\n Reached 60% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+callbacks = myCallback()
+mnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+import numpy as np
+np.set_printoptions(linewidth=200)
+import matplotlib.pyplot as plt
+plt.imshow(training_images[0])
+print(training_images[0])
+print(training_labels[0])
+
+training_images = training_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(512, activation='relu'),
+                                    tf.keras.layers.Dense(10, activation='softmax')])
+
+model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=15, callbacks=[callbacks])
+model.evaluate(test_images, test_labels)
+
+#output the probability of the item is each of the 10 classes
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
+
