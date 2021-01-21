b26deea571cbbe6cc93ec63ed44a8c4faf91a3c1
seperina <seperina.ji@gmail.com>
2021-01-21 23:34:10 +0800

    Fixed CatsDogs Augmentation issue

--- Convolutional Neural Network/Week2/ImageAugmentationCatsDogs.py
+++ Convolutional Neural Network/Week2/ImageAugmentationCatsDogs.py
@@ -38,9 +38,10 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Flatten(),
     #dropout to avoid overfitting
     tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Flatten(),
+
     tf.keras.layers.Dense(512, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
