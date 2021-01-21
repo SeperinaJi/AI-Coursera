75298f740ee839e42a70c1f0e0d4a4dd83373210
seperina <seperina.ji@gmail.com>
2021-01-21 14:14:11 +0800

    Added Week4 HorseOrHuman Binary Classification with CNN case

---
+++ Introduction To TensorFlow for Artificial Intelligence,Machine Learning and Deep Learning /Week4/HorsesOrHumans.py
@@ -0,0 +1,158 @@
+#wget --no-check-certificate \
+ #  https://storage.googleapis.com/laurencemoroney-blog.appspot.com/horse-or-human.zip \
+ #   -O /tmp/horse-or-human.zip
+
+#wget --no-check-certificate \
+ #   https://storage.googleapis.com/laurencemoroney-blog.appspot.com/validation-horse-or-human.zip \
+  #  -O /tmp/validation-horse-or-human.zip
+
+import os
+import zipfile
+
+local_zip = '/tmp/horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/horse-or-human')
+local_zip = '/tmp/validation-horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/validation-horse-or-human')
+zip_ref.close()
+
+train_horse_dir = os.path.join('/tmp/horse-or-human/horses')
+train_human_dir = os.path.join('/tmp/horse-or-human/humans')
+validation_horse_dir = os.path.join('/tmp/validation-horse-or-human/horses')
+validation_human_dir = os.path.join('/tmp/validation-horse-or-human/humans')
+
+train_horse_names = os.listdir(train_horse_dir)
+train_human_names = os.listdir(train_human_dir)
+validation_horse_names = os.listdir(validation_horse_dir)
+validation_human_names = os.listdir(validation_human_dir)
+
+print('total training horse images: ', len(train_horse_names))
+print('total training human images: ', len(train_human_names))
+print('total validation horse images: ', len(validation_horse_names))
+print('total validation horse images: ', len(validation_human_names))
+
+#display images 4x4 layout
+import  matplotlib.pyplot as plt
+import  matplotlib.image as mpimg
+
+nrows = 4
+ncols = 4
+pic_index = 0
+fig = plt.gcf()
+fig.set_size_inches(ncols * 4, nrows * 4)
+
+pic_index += 8
+next_horse_pix = [os.path.join(train_horse_dir, fname) for fname in train_horse_names[pic_index - 8 : pic_index]]
+next_human_pix = [os.path.join(train_human_dir, fname) for fname in train_human_names[pic_index - 8 : pic_index]]
+
+for i , imag_path in enumerate(next_horse_pix + next_human_pix):
+    sp = plt.subplot(nrows, ncols , i + 1)
+    sp.axis('Off')
+    img = mpimg.imread(imag_path)
+    plt.imshow(img)
+
+plt.show()
+
+import tensorflow as tf
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
+])
+
+model.summary()
+
+from tensorflow.keras.optimizers import RMSprop
+model.compile(loss='binary_crossentropy', optimizer=RMSprop(lr=0.001), metrics=['accuracy'])
+
+#flow data from director using ImageGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1/255)
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_datagen.flow_from_directory(
+    '/tmp/horse-or-human/',
+    target_size=(150, 150),
+    batch_size=128,
+    class_mode='binary')
+
+validation_generator = validation_datagen.flow_from_directory(
+    '/tmp/validation-horse-or-human/',
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary'
+)
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=8,
+    epochs=15,
+    verbose=1,
+    validation_data=validation_generator,
+    validation_steps=8
+)
+
+#visualizing the intermediate outputs
+import numpy as np
+import random
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+np.seterr(divide='ignore', invalid='ignore')
+# Let's define a new Model that will take an image as input, and will output
+# intermediate representations for all layers in the previous model after
+# the first.
+successive_outputs = [layer.output for layer in model.layers[1:]]
+#visualization_model = Model(img_input, successive_outputs)
+visualization_model = tf.keras.models.Model(inputs = model.input, outputs = successive_outputs)
+# Let's prepare a random input image from the training set.
+horse_img_files = [os.path.join(train_horse_dir, f) for f in train_horse_names]
+human_img_files = [os.path.join(train_human_dir, f) for f in train_human_names]
+img_path = random.choice(horse_img_files + human_img_files)
+
+img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
+x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
+x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+
+# Rescale by 1/255
+x /= 255
+
+# Let's run our image through our network, thus obtaining all
+# intermediate representations for this image.
+successive_feature_maps = visualization_model.predict(x)
+
+# These are the names of the layers, so can have them as part of our plot
+layer_names = [layer.name for layer in model.layers]
+
+# Now let's display our representations
+for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+  if len(feature_map.shape) == 4:
+    # Just do this for the conv / maxpool layers, not the fully-connected layers
+    n_features = feature_map.shape[-1]  # number of features in feature map
+    # The feature map has shape (1, size, size, n_features)
+    size = feature_map.shape[1]
+    # We will tile our images in this matrix
+    display_grid = np.zeros((size, size * n_features))
+    print(n_features)
+    for i in range(n_features):
+      # Postprocess the feature to make it visually palatable
+      x = feature_map[0, :, :, i]
+      x -= x.mean()
+      x /= x.std()
+      x *= 64
+      x += 128
+      x = np.clip(x, 0, 255).astype('uint8')
+      # We'll tile each filter into this big horizontal grid
+      display_grid[:, i * size : (i + 1) * size] = x
+    # Display the grid
+    scale = 20. / n_features
+    plt.figure(figsize=(scale * n_features, scale))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
